e05463b619828ec5c6db7c907ef7f1307b7e4da7
balajinelakurthi07 <balajinelakurthi77@gmail.com>
2024-02-16 23:12:33 +0530

    changes

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+import uuid
 class Student(models.Model):
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4,unique=True,editable=False)
     name = models.CharField(max_length=255)
     roll_number = models.CharField(max_length=20, unique=True)
     grade = models.CharField(max_length=1)
